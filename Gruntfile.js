347d742df9e170c98cb4da41a220ab911c86e4f0
edufromkrakoa <wikialmeida@gmail.com>
2025-05-14 16:34:31 -0300

    Ajuste

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,7 +94,7 @@ module.exports = function(grunt) {
         uglify: {
             target: {
                 files: {
-                    'dist/script/main.min.js': 'src/scripts/main.js'
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
                 }
             }
         }
